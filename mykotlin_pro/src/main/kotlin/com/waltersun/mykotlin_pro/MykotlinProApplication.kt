3595f6cd987ba673a095e81d4c929ce8c77483ac
道禹 Walter <sunwenhao.swh@alibaba-inc.com>
2022-09-07 16:09:39 +0800

    [*]kotlin project init

---
+++ mykotlin_pro/src/main/kotlin/com/waltersun/mykotlin_pro/MykotlinProApplication.kt
@@ -0,0 +1,11 @@
+package com.waltersun.mykotlin_pro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MykotlinProApplication
+
+fun main(args: Array<String>) {
+    runApplication<MykotlinProApplication>(*args)
+}
---
+++ mykotlin_pro/src/test/kotlin/com/waltersun/mykotlin_pro/MykotlinProApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.waltersun.mykotlin_pro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MykotlinProApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
