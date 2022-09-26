14de92616d5e1f86013e2a40fe0009c9b7af4f42
道禹 Walter <sunwenhao.swh@alibaba-inc.com>
2022-09-26 21:40:30 +0800

    [*]kotlin enum demo

---
+++ mykotlin_pro/src/main/kotlin/com/waltersun/KotlinTest.kt
@@ -0,0 +1,13 @@
+package com.waltersun
+
+/**
+ * @author  道禹 Walter
+ * @date    2022-09-14
+ */
+class KotlinTest {
+
+}
+
+fun main(args: Array<String>) {
+    print("Hello world!")
+}
---
+++ mykotlin_pro/src/main/kotlin/com/waltersun/mykotlin_pro/enums/TableEnum.kt
@@ -0,0 +1,24 @@
+package com.alibaba.middleware.jingwei.enums
+
+import org.jetbrains.kotlin.org.apache.commons.lang3.StringUtils
+
+
+/**
+ * @author  道禹 Walter
+ * @date    2022-09-26
+ */
+enum class TableEnum(val tableName: String, val serviceName: String) {
+    GLOBAL_USER_ADDRESS("__test_global_user_address", "globalUserAddress"),
+    GLOBAL_USER_ADDRESS_SNAPSHOT("__test_global_user_address_snapshot", "globalUserAddressSnapshot");
+
+    companion object {
+        fun getServiceName(table: String): String {
+            for (value in TableEnum.values()) {
+                if (value.tableName == table) {
+                    return value.serviceName
+                }
+            }
+            return ""
+        }
+    }
+}
